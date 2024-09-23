3915d851200a7649c8a8663bc97dd6a12654c575
Muhammad Bilal Ellahi <67937279+MuhammadBilalEllahi@users.noreply.github.com>
2024-09-23 09:22:09 +0500

    class Linked List

---
+++ task 3-Linked List-Class/LinkedList.cpp
@@ -0,0 +1,8 @@
+class LinkedList {
+    public:
+        Node head;
+        Node tail;
+}
+
+// construrctor initializer list: helps in compostion, to pass values before allocation as engine runs first before car.
+// composition: an ibject exist within an object. a car has a engine.
